69d33c1492680200a607fd90bf5a76f3e8a3cdb5
Adrian Montoya <montoya@astralta.com>
2023-10-17 13:17:07 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+
+<AppShell class="bg-[#f3f3f3] relative w-full h-full">
+	<!-- (header) -->
+	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">
+		
+		<!-- <div class="w-full h-36 absolute bottom-0 bg-green-500">
+			Footer
+		</div> -->
+		<div class="w-full h-32 bg-[#f3f3f3] flex flex-col items-center justify-center p-6">
+			Some footer text here as well aba hab kein Akku mehr
+		</div>
+	</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+
+</script>
+
+<div class="w-full h-full bg-[#f3f3f3] flex flex-col font-serif">
+	<!-- Cover image -->
+	<div class="w-full h-[550px] md:h-[650px] lg:h-[750px] bg-image bg-[url('/cover.jpg')] relative">
+		<!-- Holder for logo and name -->
+		<div class="w-full absolute bottom-6 left-0 lg:left-6 flex flex-row gap-2 p-2 md:p-4 lg:p-8 z-20">
+			<!-- Logo -->
+			<div>
+				<img src="/logo.png" alt="GegoLogo" class="w-20 px-2">
+			</div>
+			<!-- Titel -->
+			<div class=" flex flex-col text-white ">
+				<span class=" text-4xl font-serif tracking-wider">
+					Gemeinde Gottes
+				</span>
+				<span class="font-serif text-md tracking-widest">
+					Biserica Elim München
+				</span>
+			</div>
+		</div>
+
+		<!-- Black gradient -->
+		<div class="w-full h-full opacity-60 bg-gradient-to-b from-transparent to-black z-10">
+
+		</div>
+	</div>
+
+	<!-- Header + CTA -->
+	<div class="w-full h-28 p-2 text-black tracking-wider flex flex-col justify-center items-center bg-[#dddddd]">
+		<div class="text-center justify-center flex flex-col py-4">
+			<span class="text-xl font-bold">
+				Duminică - de la 10:00 la 12:00
+			</span>
+			<a href="https://maps.app.goo.gl/QsRuAWZcTrKKqX7F6">
+				<span class="text-lg font-light opacity-60 transition-all duration-300 ease-in-out hover:text-xl hover:opacity-100">
+					Ingolstädter Str. 43, 80807 München
+				</span>
+			</a>
+
+		</div>
+	</div>
+
+	<!-- Content -->
+	<div class="w-full flex flex-col md:flex-row gap-4 md:gap-6 m-2 px-8 md:px-12 lg:px-20 py-6 md:py-12 lg:py-20">
+		<div class="">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+			et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+			aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+			 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+			 officia deserunt mollit anim id est laboru
+		</div>
+
+		<div>
+			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+			 totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+			 dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+			 sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+			  qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+		</div>
+
+		
+	</div>
+
+
+</div>
